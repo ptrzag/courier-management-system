752aabb50dc92c76dd074ecb6c3aa413bad51349
ptrzag <ptrzag@users.noreply.github.com>
2025-04-21 21:09:08 +0200

    Init project

---
+++ src/main/java/pl/polsl/courier/management/system/controller/Controller.java
@@ -0,0 +1,13 @@
+package pl.polsl.courier.management.system.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, Spring Boot!";
+    }
+}
